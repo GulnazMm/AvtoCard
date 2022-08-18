ee437a86c0112a49280bee082a866284bcc2d269
Gulnaz <madam.day@mail.ru>
2022-08-18 17:56:39 +0300

    Initial commit

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,40 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import java.nio.channels.ConnectionPendingException;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class CardTest {
+
+    public String generateDate(int days){
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    String planDate = generateDate(3);
+
+    @Test
+    void BookCard() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $x("//input[@placeholder='Город']").setValue("Уфа");
+        $x("//input[@placeholder='Дата встречи']").setValue("20.08.2022");
+        $x("//input[@placeholder='Дата встречи']").setValue(planDate);
+        $("[name= 'name']").setValue("Иванова-Петрова Мария");
+        $("[name='phone']").setValue("+79174203535");
+        $x("// *[@class='checkbox__box']").click();
+        $x("// *[text()='Запланировать']").click();
+        $("[data-test-id = 'replan-notification']").should(visible,Duration.ofSeconds(10));
+        $x("//*[text()='Перепланировать']").click();
+        $$("[data-test-id=success-notification]").filter(visible).first(1);
+                /*.shouldHave(Condition.text("Успешно! Встреча успешно запланирована на" + planDate), Duration.ofSeconds(10));*/
+
+    }
+}
